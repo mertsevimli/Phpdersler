0ad7f40a9948ebce6e406cf7d6b4b0ceec625073
mertsevimli <mert234tr@gmail.com>
2025-08-07 23:54:53 +0300

    Refactor Array_chunk.php to improve code readability and organization by commenting out example code sections.

--- Array_chunk.php
+++ Array_chunk.php
@@ -7,14 +7,66 @@
 </head>
 <body>
     <?php
-// array_chunk() ile diziyi parçalara ayırma
+//* array_chunk() ile diziyi parçalara ayırma
 
-$elemanlar = array("elma", "armut", "muz", "çilek" , "kivi", "portakal", "erik", "karpuz","çilek", "kavun", "nar");
-echo "<pre>";
-print_r(array_chunk($elemanlar, 3)); 
-print_r(array_chunk($elemanlar, 3, true)); // true parametresi ile anahtarları korur
-echo "<pre>";
+// $elemanlar = array("elma", "armut", "muz", "çilek" , "kivi", "portakal", "erik", "karpuz","çilek", "kavun", "nar");
+// echo "<pre>";
+// print_r(array_chunk($elemanlar, 3)); 
+// print_r(array_chunk($elemanlar, 3, true)); // true parametresi ile anahtarları korur
+// echo "<pre>";
 
+
+//* array_combine() ile diziyi anahtar-değer çiftlerine dönüştürme
+// $elemanlar = array("elma", "armut", "muz", "çilek", "kivi" );
+// $elemanlar2 = array("audi", "bmx", "muz", "volvo" ); 
+// $olusandizi= array_combine($elemanlar, $elemanlar2);
+// echo "<pre>";
+// print_r($olusandizi); 
+// echo "<pre>";
+
+//* array flip() ile dizinin anahtarlarını ve değerlerini değiştirir
+// $elemanlar = array("nesne" => "audi", "bisiklet" => "bmx", "manav" => "muz", "araba" => "volvo" );
+// $olusandizi = array_flip($elemanlar);
+// echo "<pre>";
+// print_r($olusandizi); 
+// echo "<pre>";
+
+//* array_key_exists() ile dizide anahtarın varlığını kontrol etme arama işlemi yapma
+
+// $elemanlar = array("nesne" => "audi", "bisiklet" => "bmx", "manav" => "muz", "araba" => "volvo" );
+// if (array_key_exists("nesnasae", $elemanlar)) {
+//     echo "Nesne dizide var <br>";
+// } else {
+//     echo "Nesne dizide yok <br>";
+// }
+
+
+//* Array_map  array liste ile  fuction içerisindekileri kaç adetle çarpmak istiyorsan ekler
+
+// function islem($sayi) {
+//     return $sayi * 2;
+// }
+// $elemanlar = array("1", "2", "3", "4", "5");
+// echo "<pre>";
+// print_r(array_map("islem", $elemanlar)); // her elemanı islem fonksiyonuna gönderir
+// echo "<pre>";
+
+//* Array_merge() ile dizileri birleştirme string olarak yazılan var tamamıyla karısık
+// $elemanlar = array("model" => "T120", 8, 9 );
+// $elemanlar2 = array("zeytin", "peynir" , "model" => "z850", "araba" => "volvo",35 );
+// echo "<pre>";
+// print_r(array_merge($elemanlar,$elemanlar2)); // yeni diziyi oluşturur
+// echo "<pre>";
+
+//* Array_pop() ile dizinin son elemanını çıkarma
+
+$elemanlar = array("elma", "armut", "muz", "çilek", "kivi" );
+$olusandizi = array_pop($elemanlar); // son elemanı çıkarır
+echo "<pre>";
+print_r($olusandizi); 
+
+//** Son dizide çıkan elemanı ekrana yazdırır */
+ 
     ?>
 </body>
 </html>
