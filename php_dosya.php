06951471d1455eabac6895bb8674c7fb188da7a7
mertsevimli <mert234tr@gmail.com>
2025-09-02 14:47:33 +0300

    Update file permissions to executable for multiple PHP and JSON files; add sample text files for demonstration.

--- php_dosya.php
+++ php_dosya.php
@@ -8,10 +8,37 @@
 <body>
 
 <?php
+// $path = __DIR__ . "/Özel";
+
+// // Klasör yoksa oluştur
+// if (!is_dir($path)) {
+//     mkdir($path, 0777, true);
+// }
+
+// // Dosya oluştur/yaz
+// $file = $path . "/test.txt";
+// file_put_contents($file, "Merhaba Mert!");
+
+// echo "Klasör ve dosya hazır: $file";
 
 
+// touch("Özel/deneme.txt");
+
+// //* Var olan dosyaları okumak için açma yazmak için açma ve tipleri mevcut
+// $dosya = fopen("Özel/deneme.txt", "r+"); //*r+ okuma ve yazma
+
+// fclose($dosya);//* Dosyayı kapatır mutlaka dosyayı kapatmak gerekir.Zaman aşımı yaşanabilir.
+
+//*fread  dosya içerisindekileri görüntüler
+
+$dosya = fopen("Özel/deneme.txt", "r"); //*r okuma 
+$icerik = fread($dosya,filesize("Özel/deneme.txt"));
+echo $icerik;
+fclose($dosya);
+
+//*fwrite
 
 ?>
-    
+
 </body>
 </html>
