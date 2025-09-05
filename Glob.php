18bbce8ffd3bf39587c9b4f4b01f8788c008cc65
mertsevimli <mert234tr@gmail.com>
2025-09-05 14:39:26 +0300

    Add Glob.php for file reading examples, delete unnecessary merhaba2.txt, and create gereksiz.txt with sample content; implement basic HTML structure in require.php.

--- Glob.php
+++ Glob.php
@@ -8,8 +8,45 @@
 <body>
     <?php
 
+//*Glob ile dosya okuma (klasör içindeki tüm dosyaları okur ve listeler)
 
 
+// foreach (glob("*") as $dosya):
+//     echo '<a href="' . $dosya . '">' . $dosya . '</a><br>';
+// endforeach;
+
+//*Scandir ile dosya okuma (klasör içindeki tüm dosyaları okur ve listeler)
+
+//*Görmek istediğimiz dizini parantez içine yazabiliriz. Örnek: ("/home/user/public_html")
+//*Eğer boş bırakılırsa mevcut dizini okur.
+
+// $dosyalar = scandir(".");
+// foreach ($dosyalar as $dosya):
+
+//     if($dosya=="."|| $dosya=="." || $dosya=="..") continue;
+//     echo '<a href="' . $dosya . '">' . $dosya . '</a><br>';
+// endforeach;
+
+//*rename
+// if (file_exists("Özel/gereksiz.txt")) {
+//     echo "Dosya var";
+// } else {
+//     echo "Dosya yok";
+//     }
+
+    //*  @ işareti hatayı gizler hatayı göstermez 
+
+    
+
+// @rename("Özel/merhaba2.txt", "Özel/gereksiz.txt");
+
+// echo "content=\"width=device-width";
+//* is_file dosya mı değil mi kontrol eder. true false döner
+        // if (is_file("Özel/gereksiz.txt")) :
+        //     echo "Dosya var";
+        // else :
+        //     echo "Dosya yok";
+        // endif;
 
 
     ?>
